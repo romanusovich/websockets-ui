25a857666b1a807af573bcd5fdbd793de0ed86e0
romanusovich1 <romanusovich1@gmail.com>
2023-07-09 11:36:02 +0300

    feat: remove ability to create more than one room

--- src/ws_server/handlers/create-room.js
+++ src/ws_server/handlers/create-room.js
@@ -1,5 +1,17 @@
 import { ROOMS } from "../data/rooms.js";
 
-export function createRoom() {
-    
+export function createRoom(user) {
+    const newRoom = {
+        roomId: ROOMS.length + 1,
+        roomUsers: [
+            {
+                name: user.name,
+                index: user.index,
+            },
+        ],
+    };
+    const isCreated = ROOMS
+        .every((room) => room.roomUsers
+            .every((use) => use.index !== user.index));
+    if (isCreated) ROOMS.push(newRoom);
 }
